c3819aa59ff5f71f10a3a36f4aff33f1d017fef0
Rodolfo Escobar <r.escurib@gmail.com>
2026-09-05 19:36:54 -0600

    fix charuco detector.

--- src/tools/camera_calibration/charuco_detection.cpp
+++ src/tools/camera_calibration/charuco_detection.cpp
@@ -14,19 +14,33 @@ int main() {
     return 1;
   }
 
-  const cv::aruco::Dictionary dictionary =
-      cv::aruco::getPredefinedDictionary(cv::aruco::DICT_5X5_100);
+  // Create board that matches the one used for calibration
+  const int    squaresX      = 6;
+  const int    squaresY      = 8;
+  const float  squareLength  = 0.03f;
+  const float  markerLength  = 0.015f;
+  const auto   dictionary  = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_5X5_100);
+  const auto   board       = cv::aruco::CharucoBoard(cv::Size(squaresX, squaresY), squareLength, markerLength, dictionary);
 
-  // Create detector object with default parameters
-  cv::aruco::ArucoDetector detector(dictionary);
+  // Create detector object 
+  cv::aruco::CharucoDetector detector(board);
 
+  std::vector<cv::Point2f> charuco_corners;
   std::vector<std::vector<cv::Point2f>> marker_corners;
-  std::vector<int> marker_ids;
-  detector.detectMarkers(image, marker_corners, marker_ids);
+  std::vector<int> charuco_ids, marker_ids;
+  detector.detectBoard(image, charuco_corners, charuco_ids, marker_corners, marker_ids);
 
-  cv::aruco::drawDetectedMarkers(image, marker_corners, marker_ids);
+  if (!marker_ids.empty()) {
+    cv::aruco::drawDetectedMarkers(image, marker_corners, marker_ids);
+  }
+  if (!charuco_corners.empty()) {
+    // Yellow corners
+    cv::aruco::drawDetectedCornersCharuco(image, charuco_corners, cv::noArray(), cv::Scalar(0, 255, 255));
+  }
 
   std::cout << "Detected markers: " << marker_ids.size() << std::endl;
+  std::cout << "Detected ChArUco corners: " << charuco_ids.size() << std::endl;
+  std::cout << "Press ESC to exit" << std::endl;
   cv::imshow("ChArUco marker detection", image);
   cv::waitKey(0);
 
